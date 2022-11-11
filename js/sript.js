49c4e427acb5cf2b9c620f8f2a10014d8729630a
SaverioCascelli <saverio.cascelli@gmail.com>
2022-11-11 18:16:48 +0100

    [add] email print in html

--- js/sript.js
+++ js/sript.js
@@ -3,7 +3,30 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            msg:'we',
+            numberOfEmail:10,
+            emails:[],
+            mailUrl:"https://flynn.boolean.careers/exercises/api/random/mail",
+            isEmailRequestEnded: false,
         }
+    },
+    methods:{
+        createEmail(){
+            this.isEmailRequestEnded = false;
+            console.log(this.isEmailRequestEnded);
+            this.emails = [];
+            for(let i = 0; i < this.numberOfEmail; i++){
+                axios.get(this.mailUrl)
+                .then( result =>{
+                    this.emails.push(result.data.response);
+                    if(this.emails.length === this.numberOfEmail){
+                        this.isEmailRequestEnded = true
+                        console.log(this.isEmailRequestEnded);
+                    }
+                })
+            }
+            
+        }
+    },mounted(){
+        this.createEmail();
     }
 }).mount('#app');
